d1e07fafca8ba7eae6b335034dfb4d949e3e1c8f
Marduk <arenash8@gmail.com>
2024-09-26 00:42:05 -0400

    zksync add functionality

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 
 contract FundFundMe is Script {
     uint256 SEND_VALUE = 0.1 ether;
@@ -16,7 +16,10 @@ contract FundFundMe is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
         fundFundMe(mostRecentlyDeployed);
     }
 }
@@ -30,7 +33,10 @@ contract WithdrawFundMe is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
         withdrawFundMe(mostRecentlyDeployed);
     }
 }
